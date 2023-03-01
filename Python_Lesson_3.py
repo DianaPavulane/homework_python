aa3f0772d1e3abcbecfdde13139b8eea9fae2cd7
Diana P <diana.pavulane@gmail.com>
2023-03-02 00:14:27 +0200

    Updated file

--- Python_Lesson_3.py
+++ Python_Lesson_3.py
@@ -24,3 +24,14 @@ b = ""
 for x in range (len(a)-1, -1, -1):
     b = b + a[x]
 print(b)
+
+# Task 2.3
+def factorial(x):
+    factorial = 1
+    if x >= 0:
+        for i in range (1, x+1):
+            factorial = factorial * i
+        return factorial
+    else:
+        print("Invalid input")
+        return False
